5688d7b60351d089cd1b97b2e067a96acca93c32
Yann BAYART <ybayart@e3r12p10.42.fr>
2021-01-06 18:43:20 +0100

    unique client accepted & work

--- rendu/mini_serv_2/mini_serv.c
+++ rendu/mini_serv_2/mini_serv.c
@@ -3,14 +3,8 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
-void	fatal_error(void)
-{
-	write(2, "Fatal error\n", 12);
-}
 
 int extract_message(char **buf, char **msg)
 {
@@ -25,12 +19,9 @@ int extract_message(char **buf, char **msg)
 	{
 		if ((*buf)[i] == '\n')
 		{
-			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
-			if (newbuf == 0)
-			{
-				fatal_error();
+			newbuf = malloc(sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == NULL)
 				return (-1);
-			}
 			strcpy(newbuf, *buf + i + 1);
 			*msg = *buf;
 			(*msg)[i + 1] = 0;
@@ -52,7 +43,7 @@ char *str_join(char *buf, char *add)
 	else
 		len = strlen(buf);
 	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
-	if (newbuf == 0)
+	if (newbuf == NULL)
 		return (0);
 	newbuf[0] = 0;
 	if (buf != 0)
@@ -63,82 +54,86 @@ char *str_join(char *buf, char *add)
 }
 
 int main(int argc, char** argv) {
-	int sockfd, connfd, port, max_fd, ret;
-	socklen_t len;
-	struct sockaddr_in servaddr, cli; 
-	fd_set set_read, set_write;
+	int sockfd, connfd, max_fd, ret;
+	socklen_t	len;
 	struct timeval timeout;
-	char buf[4096];
-	ssize_t len_ssize;
+	struct sockaddr_in servaddr, cli; 
+	fd_set	set_read, set_write;
+	char	buff[4096];
+	ssize_t	ssize_len;
 
 	if (argc != 2)
 	{
-		write(2, "Wrong number of arguments\n", 26);
+		write(1, "Wrong number of arguments\n", 26); 
 		exit(1);
 	}
-	port = atoi(argv[1]);
 	// socket create and verification 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
-		fatal_error();
-		exit(1); 
+	if (sockfd == -1)
+	{
+		write(1, "Fatal error\n", 12); 
+		exit(1);
 	} 
 	bzero(&servaddr, sizeof(servaddr)); 
-
 	// assign IP, PORT 
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	servaddr.sin_port = htons(port); 
+	servaddr.sin_port = htons(atoi(argv[1]));
   
 	// Binding newly created socket to given IP and verification 
 	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
 	{ 
-		fatal_error();
-		exit(1); 
+		write(1, "Fatal error\n", 12); 
+		exit(1);
 	} 
 	if (listen(sockfd, 10) != 0)
 	{
-		fatal_error();
-		exit(1); 
+		write(1, "Fatal error\n", 12); 
+		exit(1);
 	}
 	len = sizeof(cli);
-	timeout.tv_sec = 10;
+	timeout.tv_sec = 5;
+	timeout.tv_usec = 0;
 	FD_ZERO(&set_read);
 	FD_ZERO(&set_write);
 	FD_SET(sockfd, &set_read);
 	max_fd = sockfd;
+	bzero(&buff, 4096);
+	connfd = -1;
 	while (42)
 	{
-//		write(1, "toto\n", 5);
+//		write(1, "loop\n", 5);
 		ret = select(max_fd + 1, &set_read, &set_write, NULL, &timeout);
 		if (ret > 0)
 		{
-			if (FD_ISSET(sockfd, &set_read))
+//			write(1, "here\n", 5);
+			if (FD_ISSET(sockfd, &set_read)) // new connection
 			{
+//				write(1, "bzzz\n", 5);
 				connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
-				if (connfd < 0)
-				{ 
-					fatal_error();
-					exit(1); 
-				}
-				max_fd = (max_fd < connfd ? connfd : max_fd);
+				if (connfd < 0) { 
+					write(1, "Fatal error\n", 12); 
+					exit(1);
+				} 
 				FD_SET(connfd, &set_read);
-				write(1, "connection accepted\n", 20);
+				max_fd = (max_fd < connfd ? connfd : max_fd);
+				write(1, "connected\n", 10);
 			}
-			else if (FD_ISSET(connfd, &set_read))
+			else if (connfd != -1 && FD_ISSET(connfd, &set_read))
 			{
-				len_ssize = recv(connfd, buf, 4096, 0);
-				if (len_ssize <= 0)
+				ssize_len = recv(connfd, &buff, 4096, 0);
+				if (ssize_len <= 0) // close connection
 				{
-					write(1, "connection closed\n", 18);
+					FD_ZERO(&set_read);
+					FD_SET(sockfd, &set_read);
+					max_fd = sockfd;
 					close(connfd);
+					connfd = -1;
+					write(1, "disconnected\n", 13);
 				}
 				else
-					write(1, buf, len_ssize);
+					write(1, buff, ssize_len);
 			}
-//			printf("ret: %d\n", ret);
-//			printf("sockfd: %d | %d\n", FD_ISSET(sockfd, &set_read), FD_ISSET(sockfd, &set_write));
-//			printf("connfd: %d | %d\n", FD_ISSET(connfd, &set_read), FD_ISSET(connfd, &set_write));
 		}
 	}
 }
